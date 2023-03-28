2b3cef3ae311dce3ca20bbdee9218bf231aa9e5e
Administrator <Administrator@Moons>
2023-03-28 21:43:22 +0200

    Initial Commit

---
+++ TaskA/src/breakout/BlockState.java
@@ -0,0 +1,31 @@
+package breakout;
+
+import breakout.utils.Rect;
+
+/**
+ * Represents the state of a block in the breakout.good game.
+ *
+ * TODO spec
+ */
+public class BlockState {
+	
+	/**
+	 * TODO spec
+	 */
+	private final Rect location;
+
+	/**
+	 * TODO
+	 */
+	public BlockState(Rect location) {
+		this.location = null;
+	}
+
+	/**
+	 * TODO
+	 */
+	public Rect getLocation() {
+		return null;
+	}
+	
+}
